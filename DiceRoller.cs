55bac1709a353fd0a570718a61967f69f70ae172
hyrumjackson <hyrumjjackson@gmail.com>
2024-01-17 21:56:13 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class DiceRoller
+    {
+
+        public int[] RollDice(int numDiceRolls)
+        {
+            //Declare variables
+            int die1 = 0, die2 = 0;
+            int[] diceRollArray = new int[numDiceRolls];
+
+            //Create a Random instance
+            Random random = new Random();
+
+            //For loop to roll dice and place value in array
+            for (int i = 0; i < numDiceRolls; i++)
+            {
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+
+                diceRollArray[i] = die1 + die2;
+            }
+
+            return diceRollArray;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using Mission2;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        int numDiceRolls = 0;
+        int numTotal = 0;
+        double numPercent = 0.0;
+
+        //Call DiceRoller
+        DiceRoller dr = new DiceRoller();
+
+        //Ask user for number of dice rolls
+        Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?");
+        numDiceRolls = int.Parse(Console.ReadLine());
+
+        //Roll dice
+        int [] diceRollArray = dr.RollDice(numDiceRolls);
+
+        //Display results
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS" +
+                          "\nEach \"*\" represents 1% of the total number of rolls." +
+                          "\nTotal number of rolls = " + numDiceRolls + ".\n");
+
+        //Calculate results
+        for (int i = 2; i <= 12; i++)
+        {
+            for (int j = 0; j < numDiceRolls; j++)
+            {
+                if (diceRollArray[j] == i)
+                {
+                    numTotal++;
+                }
+            }
+
+            numPercent = ((double)numTotal / numDiceRolls) * 100;
+            numPercent = Math.Round(numPercent);
+
+            if (i < 10)
+            {
+                Console.Write(i + ":  ");
+            }
+            else
+            {
+                Console.Write(i + ": ");
+            }
+
+            //Display histogram one bar at a time
+            for (int j = 0; j < numPercent; j++)
+            {
+                Console.Write("*");
+            }
+
+            //Reset numTotal and add a new line
+            numTotal = 0;
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
